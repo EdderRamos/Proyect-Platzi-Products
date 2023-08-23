2d35d6694f6948cbf2a1022776cf5f2b66807325
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-23 17:41:34 -0500

    Configuration Dagger Hilt

---
+++ app/src/main/java/com/webcontrol/platzi/PlatziStoreApp.kt
@@ -0,0 +1,8 @@
+package com.webcontrol.platzi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlatziStoreApp: Application() {
+}
--- app/src/main/java/com/webcontrol/platzi/ui/MainActivity.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.webcontrol.platzi
+package com.webcontrol.platzi.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.webcontrol.platzi.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
