121be8ba11f9119350de3936babf981dec717191
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-25 12:50:13 -0500

    VIEWS AND COLORS ARE MODIFIED

--- app/src/main/java/com/webcontrol/platzi/ui/adapters/ProductsAdapter.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/adapters/ProductsAdapter.kt
@@ -4,24 +4,32 @@ import com.webcontrol.platzi.R
 import com.webcontrol.platzi.core.ui.BaseAdapter
 import com.webcontrol.platzi.databinding.ViewProductItemBinding
 import com.webcontrol.platzi.domain.model.ProductModel
+import org.imaginativeworld.whynotimagecarousel.listener.CarouselListener
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class ProductsAdapter(
     data: List<ProductModel>,
-    private val movieListener: ProductListener
+    private val productListener: ProductListener,
 ) : BaseAdapter<ViewProductItemBinding, ProductModel>(data) {
 
     override val layoutId: Int = R.layout.view_product_item
 
     override fun bind(binding: ViewProductItemBinding, item: ProductModel) {
-         val images = mutableListOf<CarouselItem>()
+        val images = mutableListOf<CarouselItem>()
         item.images.forEach {
             images.add(CarouselItem(it, item.title))
         }
+
         binding.apply {
             product = item
-            listener = movieListener
-            carousel.addData(images)
+            listener = productListener
+            carousel.setData(images)
+            carousel.autoWidthFixing = true
+            carousel.carouselListener = object : CarouselListener {
+                override fun onClick(position: Int, carouselItem: CarouselItem) {
+                    productListener.onProductClicked(item)
+                }
+            }
             executePendingBindings()
         }
     }
