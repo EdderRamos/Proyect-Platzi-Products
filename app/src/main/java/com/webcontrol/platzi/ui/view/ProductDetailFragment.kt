672aec4c7f46ebf71cde3cfda3b181493db7ff3e
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-24 23:24:55 -0500

    ADDED SHAREDVIEWMODEL IN ProductDetailFragment

--- app/src/main/java/com/webcontrol/platzi/ui/view/ProductDetailFragment.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/view/ProductDetailFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.webcontrol.platzi.R
+import com.webcontrol.platzi.ui.SharedViewModel
 
 
 class ProductDetailFragment : Fragment() {
+
+    private val viewModel: SharedViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
