28bb80399870196b2364c628bfbcce1dd8ded413
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-24 23:22:03 -0500

    ADDED DATA BINDING IN MAINACTIVITY

--- app/src/main/java/com/webcontrol/platzi/ui/view/MainActivity.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/view/MainActivity.kt
@@ -2,13 +2,17 @@ package com.webcontrol.platzi.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.webcontrol.platzi.R
+import com.webcontrol.platzi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
