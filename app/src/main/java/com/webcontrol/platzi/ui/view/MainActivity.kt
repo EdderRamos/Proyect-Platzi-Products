ec575b27070291e913bef3f2a4014409d493b4d7
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-25 12:52:55 -0500

    FIXED AND ADDED FUNCTIONS FOR USE CASES IN VIEWMODEL

--- app/src/main/java/com/webcontrol/platzi/ui/view/MainActivity.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.webcontrol.platzi.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.webcontrol.platzi.R
 import com.webcontrol.platzi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +17,4 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-
 }
