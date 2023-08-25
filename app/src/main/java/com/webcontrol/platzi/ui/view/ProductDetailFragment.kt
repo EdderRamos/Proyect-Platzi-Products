04234a7c9ca7a1348795be7eb348c3925f15bceb
jhonatan ramos <eramos@geosatelital.com.pe>
2023-08-25 12:57:04 -0500

    THE TRANSITION IS ADDED IN PRODUCTDETAILFRAGMENT

--- app/src/main/java/com/webcontrol/platzi/ui/view/ProductDetailFragment.kt
+++ app/src/main/java/com/webcontrol/platzi/ui/view/ProductDetailFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.transition.MaterialFadeThrough
+import com.google.android.material.transition.MaterialSharedAxis
 import com.webcontrol.platzi.R
 import com.webcontrol.platzi.ui.SharedViewModel
 
@@ -16,14 +19,25 @@ class ProductDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+        returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+        exitTransition = MaterialFadeThrough()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        initUI()
         return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+
     }
 
     companion object {
